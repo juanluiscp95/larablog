5dc6ed7b0853b00623d3be0a161818b4baeb8db5
juanluiscp95 <juan.luis.calde.95@gmail.com>
2020-04-23 20:28:43 +0200

    carpetas1-4

--- resources/js/app.js
+++ resources/js/app.js
@@ -31,6 +31,6 @@ const app = new Vue({
     el: '#app',
     data: {
         message: "Hello Vue!",
-        posts:['Título 1','Título 2','Título 3','Título 4','Título 5'],
+        posts: ['Título 1','Título 2','Título 3','Título 4','Título 5'],
     }
 });
